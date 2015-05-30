5a1e5a6487344a8c9bd8de5790be0ff24cdc425f
NishaNS <nish.1012@gmail.com>
2015-05-30 12:31:27 -0700

    Level View,Controller image path updated

--- OOADProject/src/controller/LevelController.java
+++ OOADProject/src/controller/LevelController.java
@@ -25,43 +25,43 @@ public class LevelController{
 	public LevelModel levelModel;
 	private Audio auLevel;
 	private int selectionPerformed=0;
-/**
- * HelpController constructor which initializes the HelpModel and HelpView objects
- */
+	/**
+	 * HelpController constructor which initializes the HelpModel and HelpView objects
+	 */
 	public LevelController(){
-			levelModel=new LevelModel();
-			levelView=new LevelView();
-			levelView.levelController=this;
-			auLevel=new Audio("");
-			this.levelView.addPressListen(new KeyListen());
-			this.levelView.addButtonListen(new ActionListen());
-			new Thread(new Runnable() {			
-				@Override
-				public void run() {		
-					try {
-						Thread.sleep(3000);			//pause for page load + before looping
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} 
-					levelView.playSelectAudio();
-				}
-			}).start();
-}
+		levelModel=new LevelModel();
+		levelView=new LevelView();
+		levelView.levelController=this;
+		auLevel=new Audio("");
+		this.levelView.addPressListen(new KeyListen());
+		this.levelView.addButtonListen(new ActionListen());
+		new Thread(new Runnable() {			
+			@Override
+			public void run() {		
+				try {
+					Thread.sleep(3000);			//pause for page load + before looping
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} 
+				levelView.playSelectAudio();
+			}
+		}).start();
+	}
 	/**
 	 * The method used to get the View for this controller
 	 */
 	public LevelView getView(){
 		return levelView;
 	}
-	
+
 	/**
 	 * The method used to get the model for this class
 	 */
 	public LevelModel getModel(){
 		return levelModel;
 	}
-	
-	
+
+
 	/**
 	 * The getter method for selectionPerformed variable
 	 * @return int
@@ -69,7 +69,7 @@ public class LevelController{
 	public int getSelectionPerformed(){
 		return selectionPerformed;
 	}
-	
+
 	/**
 	 * The setter method for the selectionPerformed Variable
 	 * @param s
@@ -84,27 +84,27 @@ public class LevelController{
 	 */
 	private class ActionListen implements ActionListener{
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		levelView.auLevel.stopAudio();
-		if(e.getSource().equals(levelView.btnEasy)){
-			setSelectionPerformed(1);
-			levelModel.setLevel(1);
-			auLevel.setauFileName("/Users/hemali/git/FinalProject/OOADProject/audio/Level_easy_selected.wav");
-			auLevel.playAudio();
-			
-			
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			levelView.auLevel.stopAudio();
+			if(e.getSource().equals(levelView.btnEasy)){
+				setSelectionPerformed(1);
+				levelModel.setLevel(1);
+				auLevel.setauFileName("Level_easy_selected.wav");
+				auLevel.playAudio();
+
+
+			}
+			else if(e.getSource().equals(levelView.btnHard)){
+				setSelectionPerformed(1);
+				levelModel.setLevel(3);
+				auLevel.setauFileName("Level_difficult_selected.wav");
+				auLevel.playAudio();
+
+
+			}
 		}
-		else if(e.getSource().equals(levelView.btnHard)){
-			setSelectionPerformed(1);
-			levelModel.setLevel(3);
-			auLevel.setauFileName("/Users/hemali/git/FinalProject/OOADProject/audio/Level_difficult_selected.wav");
-			auLevel.playAudio();
-			
-			
-		}
-	}
-	
+
 	}
 	/**
 	 * 
@@ -112,27 +112,27 @@ public class LevelController{
 	 *
 	 */
 	public class KeyListen extends KeyAdapter{
-		
-	public void keyPressed(KeyEvent e){
-		levelView.auLevel.stopAudio();
-		
-		if(levelView.getTrackVariable()==1){
-			setSelectionPerformed(1);
-			levelModel.setLevel(1);
-			levelView.auLevel.setauFileName("/Users/hemali/git/FinalProject/OOADProject/audio/Level_easy_selected.wav");
-			levelView.auLevel.playAudio();
-			
+
+		public void keyPressed(KeyEvent e){
+			levelView.auLevel.stopAudio();
+
+			if(levelView.getTrackVariable()==1){
+				setSelectionPerformed(1);
+				levelModel.setLevel(1);
+				levelView.auLevel.setauFileName("Level_easy_selected.wav");
+				levelView.auLevel.playAudio();
+
+			}
+			else if(levelView.getTrackVariable()==2){
+				setSelectionPerformed(1);
+				levelModel.setLevel(3);
+				auLevel.setauFileName("Level_difficult_selected.wav");
+				auLevel.playAudio();
+
+			}
+
+
 		}
-		else if(levelView.getTrackVariable()==2){
-			setSelectionPerformed(1);
-			levelModel.setLevel(3);
-			auLevel.setauFileName("/Users/hemali/git/FinalProject/OOADProject/audio/Level_difficult_selected.wav");
-			auLevel.playAudio();
-			
-		}
-		
-		
-	}
 	}
 
 }
--- OOADProject/src/view/LevelView.java
+++ OOADProject/src/view/LevelView.java
@@ -31,36 +31,36 @@ public class LevelView extends JPanel{
 	public CustomButton btnHard;
 	private JLabel lblDisplay1;
 	private JLabel lblDisplay2;
-	
+
 	private String imgEasy;
 	private String imgEasySelected;
-	
+
 	private String imgHard;
 	private String imgHardSelected;
-	
-	
+
+
 	private int trackVariable;
 	public Audio auLevel;
 	public LevelController levelController;
 	public ThemeController tController;
-/**
- * LevelView constructor 
- */
-public LevelView(){
-	//if(tController.getTheme()==1)
-	{
-	//path of the farm level1 image
-	imgEasy=new String("OOADProject/image/Level1.png");
-	//path of the farm level2 image
-	imgHard=new String("/Users/hemali/git/FinalProject/OOADProject/image/Level3.png");
-	//path of the farm level1 selected image
-	imgEasySelected=new String("");
-	//path of the farm level2 selected image
-	imgHardSelected=new String("");
-	btnEasy=new CustomButton(imgEasy);
-	btnHard=new CustomButton(imgHard);
-	}
-	/*else{
+	/**
+	 * LevelView constructor 
+	 */
+	public LevelView(){
+		//if(tController.getTheme()==1)
+		{
+			//path of the farm level1 image
+			imgEasy=new String("image/Level1.png");
+			//path of the farm level2 image
+			imgHard=new String("image/Level3.png");
+			//path of the farm level1 selected image
+			imgEasySelected=new String("");
+			//path of the farm level2 selected image
+			imgHardSelected=new String("");
+			btnEasy=new CustomButton(imgEasy);
+			btnHard=new CustomButton(imgHard);
+		}
+		/*else{
 		imgEasy=new String("OOADProject/image/Level1.png");
 	imgHard=new String("/Users/hemali/git/FinalProject/OOADProject/image/Level3.png");
 	imgEasySelected=new String("");
@@ -68,153 +68,153 @@ public LevelView(){
 	btnEasy=new CustomButton(imgEasy);
 	btnHard=new CustomButton(imgHard);
 	}*/
-	lblDisplay1=new JLabel("Select the level");
-	lblDisplay2=new JLabel("(Default level is easy)");
+		lblDisplay1=new JLabel("Select the level");
+		lblDisplay2=new JLabel("(Default level is easy)");
 
-	GridBagLayout gridBag=new GridBagLayout();
-	this.setLayout(gridBag);
-	Font font=new Font("",Font.ITALIC,35);
-	
-	GridBagConstraints constraint=new GridBagConstraints();
-	constraint.gridx=0;
-	constraint.gridy=0;
-	
-	lblDisplay1.setFont(font);
-	this.add(lblDisplay1,constraint);
-	constraint.gridy++;
-	lblDisplay2.setFont(font);
-	this.add(lblDisplay2,constraint);
+		GridBagLayout gridBag=new GridBagLayout();
+		this.setLayout(gridBag);
+		Font font=new Font("",Font.ITALIC,35);
 
-	constraint.gridy++;	
-	
-	this.add(btnEasy,constraint);
-	constraint.gridx++;
-	
-	this.setName("LevelView");
-	this.add(btnHard,constraint);
-	this.setSize(200, 200);
-	this.setBackground(Color.ORANGE);
-	
-}
-/**
- * custom button class
- *
- */
-class CustomButton extends JButton{
-	String imageFile;
-	int height=400;
-	int width=400;
-	CustomButton(String m){
-		imageFile=m;
-		this.setIcon( scaleImage(height,width,new ImageIcon(imageFile)));
-	}
-	
-	public void setImageFile(String img){
-		imageFile=img;
+		GridBagConstraints constraint=new GridBagConstraints();
+		constraint.gridx=0;
+		constraint.gridy=0;
+
+		lblDisplay1.setFont(font);
+		this.add(lblDisplay1,constraint);
+		constraint.gridy++;
+		lblDisplay2.setFont(font);
+		this.add(lblDisplay2,constraint);
+
+		constraint.gridy++;	
+
+		this.add(btnEasy,constraint);
+		constraint.gridx++;
+
+		this.setName("LevelView");
+		this.add(btnHard,constraint);
+		this.setSize(200, 200);
+		this.setBackground(Color.ORANGE);
+
 	}
 	/**
-	 * The method to scale the images given to the button
+	 * custom button class
+	 *
 	 */
-	public ImageIcon scaleImage(int x,int y,ImageIcon i){
-		Image img = i.getImage(); 
-		Image newimg = img.getScaledInstance(x,y,  java.awt.Image.SCALE_SMOOTH);  
-		return new ImageIcon(newimg); 
-	}
-	@Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        this.setIcon(scaleImage(height,width,new ImageIcon(imageFile)));
-    }
-
-}
-
-/** 
- * The getter method for the private variable trackVariable
- * @return int
- * 
- */
-public int getTrackVariable(){
-	return trackVariable;
-}
-/**
- * The setter method for the private variable trackVariable
- * @param t
- */
-public void setTrackVariable(int t){
-	trackVariable=t;
-}
-/**
- * The method to add listeners to the buttons
- * @return void
- */
-public void addPressListen(LevelController.KeyListen k){
-	this.setFocusable(true);
-	this.addKeyListener(k);
-}
-/**
- * 
- * @param a
- */
-public void addButtonListen(ActionListener a){
-btnEasy.addActionListener(a);
-btnHard.addActionListener(a);
-}
-
-/**
- * The method which plays the initial audio
- * @return void
- */
-
-public void playSelectAudio(){
-	trackVariable=0;
-	while(true){
-		System.out.println("true");
-		if(levelController.getSelectionPerformed()!=1){
-			
-	auLevel=new Audio("/Users/hemali/git/FinalProject/OOADProject/audio/Level_select.wav");
-	auLevel.playAudio();
-	setTrackVariable(1);
-    
-		}else{
-			break;
+	class CustomButton extends JButton{
+		String imageFile;
+		int height=400;
+		int width=400;
+		CustomButton(String m){
+			imageFile=m;
+			this.setIcon( scaleImage(height,width,new ImageIcon(imageFile)));
 		}
-	if(levelController.getSelectionPerformed()!=1){
-		btnEasy.setBackground(Color.RED);
-        btnEasy.setOpaque(true);
-		btnEasy.repaint();
-	auLevel.setauFileName("/Users/hemali/git/FinalProject/OOADProject/audio/Level_easy_select_option.wav");
-	auLevel.playAudio();
-	btnEasy.setOpaque(false);
-	btnEasy.repaint();
+
+		public void setImageFile(String img){
+			imageFile=img;
+		}
+		/**
+		 * The method to scale the images given to the button
+		 */
+		public ImageIcon scaleImage(int x,int y,ImageIcon i){
+			Image img = i.getImage(); 
+			Image newimg = img.getScaledInstance(x,y,  java.awt.Image.SCALE_SMOOTH);  
+			return new ImageIcon(newimg); 
+		}
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			this.setIcon(scaleImage(height,width,new ImageIcon(imageFile)));
+		}
+
 	}
-	else{
-	break;	
-	}	
-	
-	if(levelController.getSelectionPerformed()!=1){
-		btnHard.setBackground(Color.RED);
-        btnHard.setOpaque(true);
-		btnHard.repaint();
-		setTrackVariable(2);
-		auLevel.setauFileName("/Users/hemali/git/FinalProject/OOADProject/audio/Level_difficult_select_option.wav");
-		auLevel.playAudio();
-		trackVariable=3;
-		btnHard.setOpaque(false);
-		btnHard.repaint();
-		continue;
-	}{
-		break;
+
+	/** 
+	 * The getter method for the private variable trackVariable
+	 * @return int
+	 * 
+	 */
+	public int getTrackVariable(){
+		return trackVariable;
 	}
+	/**
+	 * The setter method for the private variable trackVariable
+	 * @param t
+	 */
+	public void setTrackVariable(int t){
+		trackVariable=t;
 	}
-	
-}
-public static void main(String[] args) {
-	
-	LevelController levelcontroller = new LevelController();
-	JFrame window = new JFrame("Level");
-	window.add(levelcontroller.getView());		
-	window.setExtendedState(JFrame.MAXIMIZED_BOTH);
-	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	window.setVisible(true);
-}
+	/**
+	 * The method to add listeners to the buttons
+	 * @return void
+	 */
+	public void addPressListen(LevelController.KeyListen k){
+		this.setFocusable(true);
+		this.addKeyListener(k);
+	}
+	/**
+	 * 
+	 * @param a
+	 */
+	public void addButtonListen(ActionListener a){
+		btnEasy.addActionListener(a);
+		btnHard.addActionListener(a);
+	}
+
+	/**
+	 * The method which plays the initial audio
+	 * @return void
+	 */
+
+	public void playSelectAudio(){
+		trackVariable=0;
+		while(true){
+			System.out.println("true");
+			if(levelController.getSelectionPerformed()!=1){
+
+				auLevel=new Audio("Level_select.wav");
+				auLevel.playAudio();
+				setTrackVariable(1);
+
+			}else{
+				break;
+			}
+			if(levelController.getSelectionPerformed()!=1){
+				btnEasy.setBackground(Color.RED);
+				btnEasy.setOpaque(true);
+				btnEasy.repaint();
+				auLevel.setauFileName("Level_easy_select_option.wav");
+				auLevel.playAudio();
+				btnEasy.setOpaque(false);
+				btnEasy.repaint();
+			}
+			else{
+				break;	
+			}	
+
+			if(levelController.getSelectionPerformed()!=1){
+				btnHard.setBackground(Color.RED);
+				btnHard.setOpaque(true);
+				btnHard.repaint();
+				setTrackVariable(2);
+				auLevel.setauFileName("Level_difficult_select_option.wav");
+				auLevel.playAudio();
+				trackVariable=3;
+				btnHard.setOpaque(false);
+				btnHard.repaint();
+				continue;
+			}{
+				break;
+			}
+		}
+
+	}
+	public static void main(String[] args) {
+
+		LevelController levelcontroller = new LevelController();
+		JFrame window = new JFrame("Level");
+		window.add(levelcontroller.getView());		
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
 }
